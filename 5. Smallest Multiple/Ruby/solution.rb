ecc7befca6bd48ffe55ffedb4792fc0ede28da92
Spencer Tang <spencer.tang12@gmail.com>
2015-01-05 01:25:23 -0500

    Add partial solution for Ruby

--- 5. Smallest Multiple/Ruby/solution.rb
+++ 5. Smallest Multiple/Ruby/solution.rb
@@ -1,3 +1,30 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+def lowest_common_multiple(n)
+
+  prime_numbers = (2..n).select{|x| is_prime(x)}
+  # distinct_primes = prime_numbers.collect do |x|
+
+
+  # end
+end
+
+def all_factors(n)
+  factors = []
+  (2..Math.sqrt(n)).each do |x|
+    if n % x == 0
+      factors << x 
+      factors << n/x
+    end
+  end
+  factors 
+end
+
+def is_prime(n)
+  all_factors(n).empty?
+end
+
+
