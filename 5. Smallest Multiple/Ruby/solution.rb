498471568bc021782db91d704506c1b3c552c270
Spencer Tang <spencer.tang12@gmail.com>
2015-01-05 09:54:30 -0500

    Add Ruby solution

--- 5. Smallest Multiple/Ruby/solution.rb
+++ 5. Smallest Multiple/Ruby/solution.rb
@@ -3,31 +3,36 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
-def lowest_common_multiple(n)
-  distinct_primes = []
-  prime_numbers = (2..n).select{|x| is_prime(x)}
-  (2..20).each do |x|
-    prime_numbers.each |y|
-      if x % y == 0
-        
+  def lowest_common_multiple(n)
+    distinct_primes = []
+    prime_numbers = (2..n).select{|x| is_prime(x)}
+    prime_numbers.each do |x|
+      count = 1
+      primes = x
+      while(primes < 20) do 
+        if x ** count > primes 
+          primes = x ** count
+        end
+        count += 1
+      end
+      distinct_primes << primes / x
+    end
+    distinct_primes
+  end
+
+  def all_factors(n)
+    factors = []
+    (2..Math.sqrt(n)).each do |x|
+      if n % x == 0
+        factors << x 
+        factors << n/x
       end
     end
+    factors 
   end
-end
 
-def all_factors(n)
-  factors = []
-  (2..Math.sqrt(n)).each do |x|
-    if n % x == 0
-      factors << x 
-      factors << n/x
-    end
+  def is_prime(n)
+    all_factors(n).empty?
   end
-  factors 
-end
-
-def is_prime(n)
-  all_factors(n).empty?
-end
 
 
