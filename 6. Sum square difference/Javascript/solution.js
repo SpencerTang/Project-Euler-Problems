3fa6b81c507c82110c6958a0e183178bba171f0b
Spencer Tang <stang@xogrp.com>
2015-01-05 19:48:58 -0500

    Add Javascript solution

--- 6. Sum square difference/Javascript/solution.js
+++ 6. Sum square difference/Javascript/solution.js
@@ -7,3 +7,13 @@
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+  var diffOfSumOfSquares = function(n){
+    var numbers = [];
+    for(var i = 1; i <= n; i++){
+      numbers.push(i)
+    }
+    var sumOfSquares = numbers.map(function(x){ return Math.pow(x,2);}).reduce(function(x,y){return x + y;});
+    var squareOfSum = Math.pow(numbers.reduce(function(x,y){return x + y;}),2);
+    return squareOfSum - sumOfSquares;
+  };
