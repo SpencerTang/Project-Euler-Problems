6ce0bbda61eb6bc7cac071dda4310b9cb6c8f29b
Spencer Tang <stang@xogrp.com>
2015-01-05 20:11:55 -0500

    Add Ruby Solution

--- 6. Sum square difference/Ruby/solution.rb
+++ 6. Sum square difference/Ruby/solution.rb
@@ -7,3 +7,13 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+  def diff_of_sum_of_squares(n)
+    sum = 0
+    square = 0
+    (1..n).each do |x|
+      sum += x
+      square += (x * x)
+    end
+    (sum * sum) - square
+  end
