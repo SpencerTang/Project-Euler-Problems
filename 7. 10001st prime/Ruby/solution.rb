a68a12edda0eac905991bb6ffc33c544dd787d46
Spencer Tang <spencer.tang12@gmail.com>
2015-01-08 10:40:03 -0500

    Add Ruby solution

--- 7. 10001st prime/Ruby/solution.rb
+++ 7. 10001st prime/Ruby/solution.rb
@@ -1,17 +1,26 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-  def find_prime(n)
-    primes = []
+# What is the 10 001st prime number?
+
+  def nth_prime(n)
     count = 2
-    while primes.length < n
-      if prime?(count)
-        primes << count
+    prime_count = 0
+    prime = 2
+    while prime_count < n
+      if prime? count 
+        prime = count
+        prime_count += 1
       end
       count += 1
     end
-    primes.last
+    prime
   end
 
   def prime?(n)
-    non_primes = (2..Math.sqrt(n)).select{|x| n % x == 0}
-    non_primes.empty?
+    non_primes = (2..Math.sqrt(n)).collect do |x|
+      if n % x == 0
+        return false
+      end
+    end
+    true
   end
