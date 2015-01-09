8a60d8efe8f522eca2390571bbb8b531bfdc88d1
Spencer Tang <spencer.tang12@gmail.com>
2015-01-09 11:43:04 -0500

    Add Ruby Solution

--- 9. Special Pythagorean triplet/Ruby/solution.rb
+++ 9. Special Pythagorean triplet/Ruby/solution.rb
@@ -5,3 +5,19 @@
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+  def find_pythagorean_triplet
+    number = 500
+    a = 1
+    b = 1
+    (1..number).each do |x|
+      (1..number).each do |y|
+        if (x + y) + Math.sqrt(x ** 2 + y **2) == 1000 
+          a = x
+          b = y
+        end
+      end
+    end
+    c = Math.sqrt(a ** 2 + b **2).to_i
+    c * a * b
+  end
