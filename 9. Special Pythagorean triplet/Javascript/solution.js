59b18318eaa511d9d6eb4e231c213f9c8ad2a5ad
Spencer Tang <spencer.tang12@gmail.com>
2015-01-09 20:30:57 -0500

    Add javascript solution

--- 9. Special Pythagorean triplet/Javascript/solution.js
+++ 9. Special Pythagorean triplet/Javascript/solution.js
@@ -5,3 +5,20 @@
 
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
+
+  var findPythagorean = function(){
+    var a = 1;
+    var b = 1; 
+    var c;
+
+    for(var i = 1; i <= 1000; i++){
+      for(var n = 1; n <= 1000; n++){
+        if((i + n) + Math.sqrt(Math.pow(i,2) + Math.pow(n,2)) === 1000){
+          a = i;
+          b = n;
+        }
+      }
+    }
+    c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    return (a * b * c);
+  };
