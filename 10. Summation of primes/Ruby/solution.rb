bb8d17d2e8d0694ab27da361d1dd4813fcc71692
Spencer Tang <spencer.tang12@gmail.com>
2015-01-11 23:42:54 -0500

    Add Ruby Solution

--- 10. Summation of primes/Ruby/solution.rb
+++ 10. Summation of primes/Ruby/solution.rb
@@ -1,3 +1,22 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
+
+  def sum_of_all_primes(n)
+    sum = 0
+    (1..n).each do |x|
+      if prime? x
+        sum += x
+      end 
+    end 
+    sum
+  end
+
+  def prime?(n)
+    (2..Math.sqrt(n)).each do |x|
+      if n % x == 0
+        return false
+      end
+    end
+    true
+  end
