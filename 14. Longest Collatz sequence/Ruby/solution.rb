80411b36a2ffe42436a09b67425834104d60fbc5
Spencer Tang <spencer.tang12@gmail.com>
2015-01-20 10:20:21 -0500

    Add Ruby solution

--- 14. Longest Collatz sequence/Ruby/solution.rb
+++ 14. Longest Collatz sequence/Ruby/solution.rb
@@ -11,3 +11,28 @@
 # Which starting number, under one million, produces the longest chain?
 
 # NOTE: Once the chain starts the terms are allowed to go above one million.
+
+
+def largest_collatz
+  collatz = {0 => 0}
+  (2..1000000).each do |x|
+    make_collatz(x,collatz)
+  end
+ collatz.sort_by{|x,y| y}.last.first
+end
+
+def make_collatz(n,collatz) 
+ original = n
+ collatz_count = 0
+ while n != 1
+   if collatz[n]
+    return collatz[original] = collatz[n] + collatz_count 
+   elsif n.even?
+    n = n/2
+   elsif n.odd?
+    n = (3 * n) + 1
+   end
+   collatz_count += 1
+ end
+ collatz[original] = collatz_count
+end
