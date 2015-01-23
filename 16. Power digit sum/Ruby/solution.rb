95583dfc3e3675f0c2a6a03030fb5394650cb2fd
Spencer Tang <spencer.tang12@gmail.com>
2015-01-22 21:58:37 -0500

    Add Ruby Solution

--- 16. Power digit sum/Ruby/solution.rb
+++ 16. Power digit sum/Ruby/solution.rb
@@ -1,3 +1,8 @@
 # 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 # What is the sum of the digits of the number 21000?
+
+def sum_digits
+  number = 2 ** 1000
+  number.to_s.split("").inject{|x,y| x.to_i + y.to_i}
+end
